b6c03055663e362fd73b5d66fd0a19387d2c5ac7
j-goodman <johncurtisgoodman@gmail.com>
2026-08-04 20:25:16 -0400

    Add Dijkstra's Algorithm solution for finding the distance between two nodes.

--- problems.js
+++ problems.js
@@ -2,10 +2,30 @@
 // they are not. For example: isAdjacent(montreal, ottawa) returns true, but
 // isAdjacent(burlington, quebec) returns false
 isAdjacent = (firstCity, secondCity) => {
-
+    return firstCity.connections.includes(secondCity)
 }
 
 // Return the number of nodes in between the first city and the second.
 distanceBetween = (firstCity, secondCity) => {
+    const queue = [{city: firstCity, distanceAway: 0}]
+    const checked = {}
 
+    while (queue.length > 0) {
+        let current = queue.shift()
+        let currentCity = current.city
+        let depth = current.distanceAway
+        checked[currentCity.name] = true
+        if (currentCity.name === secondCity.name) {
+            return depth - 1
+        }
+        currentCity.connections.forEach(city => {
+            if (!checked[city.name]) {
+                queue.push({city: city, distanceAway: depth + 1})
+            }
+        })
+    }
 }
+
+console.log(
+    distanceBetween(kingston, quebec)
+)
